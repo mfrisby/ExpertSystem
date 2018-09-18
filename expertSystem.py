8a1b89e23384dfcdd4dd0b7d75defd2a2e442d34
mfrisby <mfrisby@student.42.fr>
2018-09-18 13:57:13 +0200

    fix global

--- expertSystem.py
+++ expertSystem.py
@@ -10,18 +10,21 @@ Facts = {}
 
 #Parse File	
 def removeComment(line):
-		index = 0
-		if '#' in line:
-			index = line.index("#")
-			return line[:index]
-		return line
+	index = 0
+	if '#' in line:
+		index = line.index("#")
+		return line[:index]
+	return line
 	
 def parseFile(f):
+	global Rules
+	global Query
+	global Facts
 	for line in f:
 		if line.startswith('#'):
 			continue
 		elif line.startswith('?'):
-			Query = removeComment(line)
+			Query =  removeComment(line)
 		elif line.startswith('='):
 			facts = removeComment(line)
 			for letter in facts:
@@ -31,7 +34,6 @@ def parseFile(f):
 			Rules.append(removeComment(line))
 	print(colored(str(Query), 'yellow'), '\n', colored(str(Facts), 'red'), '\n', colored(str(Rules), 'green'))
 
-	
 #priority
 # ( ) 1	
 # ! not 2
